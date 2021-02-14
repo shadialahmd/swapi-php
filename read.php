2157a6f0e45b26908fa9a63c3253ad529542942e
shadialahmd <shadi.alahmd@gmail.com>
2021-02-14 13:18:14 +0400

    Create read.php

---
+++ read.php
@@ -0,0 +1,32 @@
+<?php
+
+header("Content-type:application/json");
+
+
+class Read{
+
+    private $url="https://swapi.dev/api/people/";
+
+
+
+    public function getall(){
+
+        $curl=curl_init();
+        curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($curl,CURLOPT_URL,$this->url);
+        $result=curl_exec($curl);
+
+       // return $arr=json_decode($result,true); //return data as array or object
+        return $result;
+
+
+    }
+
+}
+
+$r=new Read();
+print_r($r->getall());
+
+
+
+?>
